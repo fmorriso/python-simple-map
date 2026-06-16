65d2c9ace9c3f9b0014c34650c7dc68a75ddf751
Fred Morrison <frederick.morrison@outlook.com>
2026-06-16 14:42:25 -0600

    show package names and versions in console when program starts

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 import folium
 import io
 import json
+
+from importlib.metadata import version
 from pathlib import Path
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWebEngineWidgets import QWebEngineView
@@ -14,6 +16,34 @@ from program_settings import ProgramSettings
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
+
+def get_package_version(pkg_name: str) -> str:
+    """
+    returns the version number of the given package name
+    :param pkg_name: the name of the package
+    :type pkg_name: string (str)
+    :return: package version number.
+    :rtype: string (str)
+    """
+    return version(pkg_name)
+
+def get_required_package_names() -> list[str]:
+    """
+    read the requirements.txt file and return a sorted list of package names.
+    :return: sorted list of package names
+    :rtype: list[str
+    """
+    packages: list[str] = []
+    with open('requirements.txt') as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue  # skip blank lines and comments
+            package = line.split('~')[0].strip()  # works for ~=, >=, ==, etc.
+            packages.append(package)
+
+    packages.sort(key = str.lower)
+    return packages
 
 def get_bounding_box_coordinates(file_path: str) -> BoundingBoxPolygon:
     with open(file_path) as f:
@@ -55,6 +85,15 @@ def run_user_interface(html_content: str) -> None:
 
 
 def main() -> None:
+    package_names = get_required_package_names()
+
+    for pkg in package_names:
+        package_name = f'{pkg}'.ljust(16)
+        try:
+            print(f'{package_name}{get_package_version(pkg)}')
+        except Exception as e:
+            print(e)
+
     bounding_box_file = ProgramSettings.get_setting("COORDINATES_FILE")
     polygon_coords = get_bounding_box_coordinates(bounding_box_file)
 
