8d6b85dc33d7df54d3b709ad97da1a02b5d5005e
Fred Morrison <frederick.morrison@outlook.com>
2026-09-23 14:21:09 -0600

    use best practices inside method set_setting

--- src/program_settings.py
+++ src/program_settings.py
@@ -2,23 +2,23 @@ import os
 from dataclasses import dataclass
 from typing import ClassVar
 
-from dotenv import load_dotenv, set_key
+from dotenv import load_dotenv, set_key, find_dotenv
 from pathlib import Path
 
 
 @dataclass(frozen = True)
 class ProgramSettings:
-    ENV_PATH: ClassVar[os.PathLike[str]] =  Path('../.env')
 
 
     @staticmethod
-    def get_setting(key: str) -> str:
-        load_dotenv(dotenv_path = ProgramSettings.ENV_PATH)
+    def get_setting(key: str) -> str | None:
+        load_dotenv()
         return os.environ.get(key)
 
 
     @staticmethod
     def set_setting(key: str, value: str) -> None:
-        load_dotenv(dotenv_path = ProgramSettings.ENV_PATH)
+        dotenv_path = find_dotenv()
+        load_dotenv(dotenv_path)
         os.environ[key] = value
-        set_key(ProgramSettings.ENV_PATH, key, value)
+        set_key(dotenv_path, key, value)
