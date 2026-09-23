7ff3d2f641cca139c395a26cd29376c6e79090d8
Fred Morrison <frederick.morrison@outlook.com>
2026-09-23 14:32:57 -0600

    raise an error if no .env file is present.

--- src/program_settings.py
+++ src/program_settings.py
@@ -1,12 +1,9 @@
 import os
 from dataclasses import dataclass
-from typing import ClassVar
-
 from dotenv import load_dotenv, set_key, find_dotenv
-from pathlib import Path
 
 # Run once when the module is imported
-DOTENV_PATH = find_dotenv()
+DOTENV_PATH = find_dotenv(raise_error_if_not_found=True)
 if DOTENV_PATH:
     load_dotenv(DOTENV_PATH, override=True)
 
